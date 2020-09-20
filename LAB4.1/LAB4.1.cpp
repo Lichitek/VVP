5d6cbc18a2b5ad31b4a9de480dad076a9dd51469
Goga Bodrov <mermaiddeath@yandex.ru>
2020-09-20 16:24:43 +0300

    Лабораторные 4

---
+++ LAB4.1/LAB4.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <math.h>
+main()
+{
+	int a, b, P, S;
+	scanf_s("%d%d", &a, &b);
+	P = 2 * (a + b);
+	S = a * b;
+	printf("%d\n%d\n", P, S);
+	return 0;
+}
---
+++ LAB4.4/LAB4.4.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float a, b, del, sum, pr, ras;
+	scanf_s("%f%f", &a, &b);
+	sum = a * a + b * b;
+	ras = a * a - b * b;
+	pr = a * a * b * b;
+	del = (a * a) / (b * b);
+	printf("%.0f\n%.0f\n", sum, ras);
+	printf("%.0f\n%.3f\n", pr, del);
+	return 0;
+}
